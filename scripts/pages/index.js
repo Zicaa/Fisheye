f881c020fc87123d12490c1b3116df5af0046beb
Nausicaa Cresta <nausicaa.cresta@gmail.com>
2023-01-25 12:14:18 +0100

    Update index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-    async function getPhotographers() {
+    /*async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
@@ -24,6 +24,30 @@
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
+    }*/
+
+    const data = './data/photographers.json';
+
+    let zone = document.getElementsByClassName("photographer_section");
+
+    // Je créé une fonction qui affiche tous les photographes
+    async function getPhotographers() {
+        try {
+            // Je récupère les données présentes dans mon fichier json
+            fetch(data)
+            // A la réponse de l'API, je récupère les données présentes dans le format json
+            .then(response => response.json())
+            // Je retourne les données
+            return data
+            // Je definie une erreur si le fetch ne fonctionne pas
+        } catch (error) {
+            console.error(error)
+            const errorElement = document.createElement('h2')
+            errorElement.classList.add('photographers_error')
+            errorElement.textContent = 'Erreur lors de la récupération des données des photographes.'
+            main.appendChild(errorElement)
+            return { photographers: [] }
+        }
     }
 
     async function displayData(photographers) {

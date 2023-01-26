e88720e41af9771d5ddf6f91363defe3f1b8bfbd
Nausicaa Cresta <nausicaa.cresta@gmail.com>
2023-01-26 15:38:26 +0100

    integration_des_liens

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,29 +1,57 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        // Je crée l'article à intégrer dans le HTML 
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const location = document.createElement( 'p' );
-        location.textContent = city + ", " + country;
-        location.classList.add("location");
-        const presentation = document.createElement( 'p' );
-        presentation.textContent = tagline;
-        location.classList.add("presentation");
-        const priceForDay = document.createElement( 'p' );
-        priceForDay.textContent = price + "€/jour";
-        location.classList.add("price");
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(presentation);
-        article.appendChild(priceForDay);
+        article.classList.add("photographer-card");
+
+        // Je crée les liens vers le portfolio des photographes
+        const photographerCard = document.createElement("a");
+        photographerCard.classList.add("photographer-card-link");
+        photographerCard.setAttribute("href", `photographer.html?id=${id}`);
+        photographerCard.setAttribute(
+        "aria-label", "Lien vers le portfolio de " + name,
+        );
+        
+        // Je crée les photos de profil des photographes + accessibilité
+        const photographerImg = document.createElement( 'img' );
+        photographerImg.classList.add("photographer-img");
+        photographerImg.setAttribute("src", picture)
+        photographerImg.setAttribute("alt", "Photo de " + name);
+
+        // J'ajoute les noms des photographes
+        const photographerName = document.createElement( 'h2' );
+        photographerName.textContent = name;
+
+        // J'ajoute leur provenance
+        const photographerLocation = document.createElement( 'p' );
+        photographerLocation.textContent = city + ", " + country;
+        photographerLocation.classList.add("location");
+
+        // J'ajoute leur presentation
+        const photographerTagline = document.createElement( 'p' );
+        photographerTagline.textContent = tagline;
+        photographerTagline.classList.add("presentation");
+
+        // J'ajoute leur prix par jour
+        const photographerPrice = document.createElement( 'p' );
+        photographerPrice.textContent = price + "€/jour";
+        photographerPrice.classList.add("price");
+
+        // J'intègre les liens dans mes cards de photographes
+        photographerCard.appendChild(photographerImg);
+        photographerCard.appendChild(photographerName);
+        
+        // J'intègre les données crées dans le HTML grâce à des appenChild
+        article.appendChild(photographerCard);
+        article.appendChild(photographerLocation);
+        article.appendChild(photographerTagline);
+        article.appendChild(photographerPrice);
         return (article);
     }
+    // Je retourne la fonction crée avec tous les éléments
     return { getUserCardDOM }
 }
